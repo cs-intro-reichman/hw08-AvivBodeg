b1a944ce093526f268a35556e571d9ca921a760f
Aviv Bodeg <AVIVBodeg@gmail.com>
2024-02-08 15:28:22 +0200

    first commit

--- Track.java
+++ Track.java
@@ -1,40 +1,56 @@
-/** Represents a music track. A track has a title (String), an artist (String), 
- *  and a duration (int), in seconds. */
+/**
+ * Represents a music track. A track has a title (String), an artist (String),
+ * and a duration (int), in seconds.
+ */
 class Track {
     private String title;
     private String artist;
     private int duration;
 
-    /** Constructs a track from the given values. */
+    /**
+     * Constructs a track from the given values.
+     */
     public Track(String title, String artist, int duration) {
         this.title = title;
         this.artist = artist;
         this.duration = duration;
     }
 
-    /** Returns this track's data as "artist, title, minutes:seconds".
-     *  For example, "John Lennon, Imagine, 3:07" */
+    /**
+     * Returns this track's data as "artist, title, minutes:seconds".
+     * For example, "John Lennon, Imagine, 3:07"
+     */
     public String toString() {
         //// Replace the following statement with code that returns
         //// the data of this track according to the method's documentation.
-        return artist + ", " + title + ", " + duration;
+        return artist + ", " + title + ", " + formattedDuration(duration);
     }
 
-    /** Returns this track's title. */
+    /**
+     * Returns this track's title.
+     */
     public String getTitle() {
         return title;
     }
-    /** Returns this track's artist. */
+
+    /**
+     * Returns this track's artist.
+     */
     public String getArtist() {
         return artist;
     }
-    /** Returns this track's duration. */
+
+    /**
+     * Returns this track's duration.
+     */
     public int getDuration() {
         return duration;
     }
 
-    /** If this track's duration is shorter than the other track's duration
-     *  returns true; otherwise returns false. */
+    /**
+     * If this track's duration is shorter than the other track's duration
+     * returns true; otherwise returns false.
+     */
     public boolean isShorterThan(Track other) {
         return duration < other.duration;
     }
@@ -42,7 +58,11 @@ class Track {
     // Returns a string that represents the totalSeconds as "minutes:seconds",
     // Where seconds is always two digits. For example, "3:17" or "12:05".
     private String formattedDuration(int totalSeconds) {
-        //// replace the following statement with your code
-        return "";
+        int minutes = totalSeconds % 60;
+        int seconds = totalSeconds - minutes * 60;
+        if (seconds < 10) {
+            return minutes + ":0" + seconds;
+        }
+        return minutes + ":" + seconds;
     }
 }
